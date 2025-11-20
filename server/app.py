6e66e3aecc44c6db93bfacb785efa12ea27667e7
stlmargy <stellahmargy0211@gmail.com>
2025-11-20 12:17:41 +0300

    extended extensions to app and fixed environment variable missing bug

--- server/app.py
+++ server/app.py
@@ -1,7 +1,16 @@
 from flask import Flask
-
+from .config import Config
+from .extensions import db,migrate
 #create flask app instance
 app=Flask(__name__)
+
+#set configuration in app
+app.config.from_object(Config)
+
+#configure db with app
+db.init_app(app)
+#configure migrate with app and db
+migrate.init_app(db=db,app=app)
 
 # Run flask app 
 if __name__ == "__main__":
--- server/config.py
+++ server/config.py
@@ -1,3 +1,7 @@
-
+from dotenv import load_dotenv
+import os
+#Call load_dotenv function from dotenv module to load environment variables in env in os
+load_dotenv()
 class Config:
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") #use os module to get database uri value 
+    
