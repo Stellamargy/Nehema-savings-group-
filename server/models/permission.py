e8f3d0f92902272e7ab23fb2d2e3c827e2bbfa73
stlmargy <stellahmargy0211@gmail.com>
2025-12-01 17:40:00 +0300

    defines permission model

--- server/models/permission.py
+++ server/models/permission.py
@@ -1,2 +1,6 @@
-class Permission:
-    pass
+from sqlalchemy import Column,Integer,String,Text
+from .database_extensions import db
+class Permission(db.Model):
+    id=Column(Integer,primary_key=True)
+    code=Column(String,nullable=False)
+    description=Column(Text,nullable=False)
