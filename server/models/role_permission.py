fe1a3a16867b40e12b001f9bccb540ef4021e3c3
stlmargy <stellahmargy0211@gmail.com>
2025-12-02 08:19:53 +0300

    adds FK constraint

--- server/models/role_permission.py
+++ server/models/role_permission.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Table, Column ,Integer
-role_permission=Table(
-    "roles_permissions",
-    Column("role_id",Integer ,primary_key=True),
-    Column("permission_id",Integer ,primary_key=True)
+from sqlalchemy import Table, Column ,Integer, ForeignKey
+role_permissions=Table(
+    "role_permissions",#table name manually , no class mapping
+    #column name , column data type , constraints
+    Column("role_id",Integer ,ForeignKey("roles.id"), primary_key=True),
+    Column("permission_id",Integer ,ForeignKey("permissions.id"),primary_key=True)
 )
