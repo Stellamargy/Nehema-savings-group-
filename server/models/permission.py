e75d22fb0598d79e37640cf5293df7652ef6ace4
stlmargy <stellahmargy0211@gmail.com>
2025-12-02 08:35:23 +0300

    adds pythonic relationship

--- server/models/permission.py
+++ server/models/permission.py
@@ -1,6 +1,10 @@
 from sqlalchemy import Column,Integer,String,Text
 from .database_extensions import db
+from sqlalchemy.orm import relationship
 class Permission(db.Model):
     id=Column(Integer,primary_key=True)
     code=Column(String,nullable=False)
     description=Column(Text,nullable=False)
+
+    #pythonic relationship
+    roles=relationship("Role",back_populates="permissions",secondary="role_permissions")
