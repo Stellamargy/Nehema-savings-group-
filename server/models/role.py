4d03dadfbb45b59dee4d8831c63b66dac2e49840
stlmargy <stellahmargy0211@gmail.com>
2025-12-02 08:30:46 +0300

    adds pythonic relationship

--- server/models/role.py
+++ server/models/role.py
@@ -1,6 +1,13 @@
 from .database_extensions import db
 from sqlalchemy import Column,Integer,String,Text
+from sqlalchemy.orm import relationship
+
 class Role(db.Model):
+    #data type , constraints
     id=Column(Integer,primary_key=True)
     name=Column(String,nullable=False)
     description=Column(Text)
+
+    # define pythonic relationships
+    #name of the model , back_populates ,
+    permissions=relationship("Permission",back_populates="roles",secondary="role_permissions")
